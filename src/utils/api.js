28c66d451e60b4756bdfd6ffe763e0e24b472818
Sprinkhaan <84741733+Sprinkhaan@users.noreply.github.com>
2025-03-11 00:42:42 +0100

    Changed db link

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,29 +1,31 @@
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
 export const fetchPersonalInfo = async () => {
-  const response = await fetch('http://localhost:8081/api/personal-info');
+  const response = await fetch(`${API_BASE_URL}/api/personal-info`);
   return response.json();
 };
 
 export const fetchProfileSummary = async () => {
-  const response = await fetch('http://localhost:8081/api/profile-summary/1');
+  const response = await fetch(`${API_BASE_URL}/api/profile-summary/1`);
   return response.json();
 };
 
 export const fetchWorkExperience = async () => {
-  const response = await fetch('http://localhost:8081/api/experience/work-experience');
+  const response = await fetch(`${API_BASE_URL}/api/experience/work-experience`);
   return response.json();
 };
 
 export const fetchEducation = async () => {
-  const response = await fetch('http://localhost:8081/api/experience/education');
+  const response = await fetch(`${API_BASE_URL}/api/experience/education`);
   return response.json();
 };
 
 export const fetchSkills = async () => {
-  const response = await fetch('http://localhost:8081/api/experience/skills');
+  const response = await fetch(`${API_BASE_URL}/api/experience/skills`);
   return response.json();
 };
 
 export const fetchProjects = async () => {
-  const response = await fetch('http://localhost:8081/api/projects');
+  const response = await fetch(`${API_BASE_URL}/api/projects`);
   return response.json();
 };
